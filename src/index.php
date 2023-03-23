95f90a0ef3b6c60847ae4189ed0cdc3cbc9b7446
Manish Kumar <105111087+devopssofteng@users.noreply.github.com>
2023-03-23 14:24:28 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,11 +1,41 @@
-<?php
+<html>
+ <head>
+  <title>Display data of Person table</title>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    <?php 
+        declare(strict_types=1);
+        echo "<h1>Display data of Person table</h1>";     
+    ?>
 
-declare(strict_types=1);
+    <?php
+    $conn = mysqli_connect('mysql', 'root', 'root', 'myDb');
+      
+    $query = 'SELECT * From Person';
+    $result = mysqli_query($conn, $query);
 
-//phpinfo();
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
+        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+        foreach($value as $element){
+            echo '<td>' . $element . '</td>';
+        }
 
-//echo "Hello World";
+        echo '</tr>';
+    }
+    echo '</table>';
 
-$connection = mysqli_connect('mysql', 'root', 'root');
+    $result->close();
 
-$connection->query('CREATE DATABASE `lemp_docker`');
+    mysqli_close($conn);
+
+    ?>
+    </div>
+</body>
+</html>
